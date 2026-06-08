f511adbc9c3e7eb8383b5c8f4ef8fb63ca07638e
aman kishore <amankishore035@gmail.com>
2026-06-08 22:57:31 +0530

    DAY-3/Q-1.c

--- DAY-3/Q-1.c
+++ DAY-3/Q-1.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 int check_prime(int n1){
     int c=0;
-    for (int i=2;i<n1;i++){
+    for (int i=1; i<=n1; i++){
         if (n1%i==0){
             c++;
         }
-        else{
-            continue;
-        }
+    else{
+        continue;
+    }
     }
     if (c==0){
-        printf("The given number  is  prime");
+        printf("The given number is prime");
     }
     else{
         printf("The given number is not prime");
     }
 }
 int main(){
-    int n,c=0;
-    printf("Enter the number to check  it is prime or not: ");
+    int n;
+    printf("Enter the number to check whether it is prime or not: ");
     scanf("%d",&n);
-    if (n==0||n==1){
-        printf("The given number is not prime");
-    }
-    else{
     check_prime(n);
-    }
     return 0;
-
 }
